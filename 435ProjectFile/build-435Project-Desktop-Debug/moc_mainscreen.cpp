df19a25af9a25ba6daf36f94983d30859c7c9468
abdallah dandashli <ayd06@mail.aub.edu>
2019-10-13 17:05:52 +0300

    updating entire file

--- 435ProjectFile/build-435Project-Desktop-Debug/moc_mainscreen.cpp
+++ 435ProjectFile/build-435Project-Desktop-Debug/moc_mainscreen.cpp
@@ -1,16 +1,16 @@
 /****************************************************************************
-** Meta object code from reading C++ file 'mainmenu_widget.h'
+** Meta object code from reading C++ file 'mainscreen.h'
 **
 ** Created by: The Qt Meta Object Compiler version 67 (Qt 5.2.1)
 **
 ** WARNING! All changes made in this file will be lost!
 *****************************************************************************/
 
-#include "../435Project/mainmenu_widget.h"
+#include "../435Project/mainscreen.h"
 #include <QtCore/qbytearray.h>
 #include <QtCore/qmetatype.h>
 #if !defined(Q_MOC_OUTPUT_REVISION)
-#error "The header file 'mainmenu_widget.h' doesn't include <QObject>."
+#error "The header file 'mainscreen.h' doesn't include <QObject>."
 #elif Q_MOC_OUTPUT_REVISION != 67
 #error "This file was generated using the moc from 5.2.1. It"
 #error "cannot be used with the include files from this version of Qt."
@@ -18,34 +18,37 @@
 #endif
 
 QT_BEGIN_MOC_NAMESPACE
-struct qt_meta_stringdata_MainMenu_Widget_t {
-    QByteArrayData data[4];
-    char stringdata[54];
+struct qt_meta_stringdata_MainScreen_t {
+    QByteArrayData data[7];
+    char stringdata[52];
 };
 #define QT_MOC_LITERAL(idx, ofs, len) \
     Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
-    offsetof(qt_meta_stringdata_MainMenu_Widget_t, stringdata) + ofs \
+    offsetof(qt_meta_stringdata_MainScreen_t, stringdata) + ofs \
         - idx * sizeof(QByteArrayData) \
     )
-static const qt_meta_stringdata_MainMenu_Widget_t qt_meta_stringdata_MainMenu_Widget = {
+static const qt_meta_stringdata_MainScreen_t qt_meta_stringdata_MainScreen = {
     {
-QT_MOC_LITERAL(0, 0, 15),
-QT_MOC_LITERAL(1, 16, 18),
-QT_MOC_LITERAL(2, 35, 0),
-QT_MOC_LITERAL(3, 36, 16)
+QT_MOC_LITERAL(0, 0, 10),
+QT_MOC_LITERAL(1, 11, 7),
+QT_MOC_LITERAL(2, 19, 0),
+QT_MOC_LITERAL(3, 20, 7),
+QT_MOC_LITERAL(4, 28, 12),
+QT_MOC_LITERAL(5, 41, 7),
+QT_MOC_LITERAL(6, 49, 1)
     },
-    "MainMenu_Widget\0Open_GameSelection\0\0"
-    "OpenRegisterMenu\0"
+    "MainScreen\0GotoTwo\0\0GotoOne\0GotoSettings\0"
+    "setUser\0u\0"
 };
 #undef QT_MOC_LITERAL
 
-static const uint qt_meta_data_MainMenu_Widget[] = {
+static const uint qt_meta_data_MainScreen[] = {
 
  // content:
        7,       // revision
        0,       // classname
        0,    0, // classinfo
-       2,   14, // methods
+       4,   14, // methods
        0,    0, // properties
        0,    0, // enums/sets
        0,    0, // constructors
@@ -53,61 +56,66 @@ static const uint qt_meta_data_MainMenu_Widget[] = {
        0,       // signalCount
 
  // slots: name, argc, parameters, tag, flags
-       1,    0,   24,    2, 0x0a,
-       3,    0,   25,    2, 0x0a,
+       1,    0,   34,    2, 0x0a,
+       3,    0,   35,    2, 0x0a,
+       4,    0,   36,    2, 0x0a,
+       5,    1,   37,    2, 0x0a,
 
  // slots: parameters
     QMetaType::Void,
     QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void, QMetaType::QString,    6,
 
        0        // eod
 };
 
-void MainMenu_Widget::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+void MainScreen::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
 {
     if (_c == QMetaObject::InvokeMetaMethod) {
-        MainMenu_Widget *_t = static_cast<MainMenu_Widget *>(_o);
+        MainScreen *_t = static_cast<MainScreen *>(_o);
         switch (_id) {
-        case 0: _t->Open_GameSelection(); break;
-        case 1: _t->OpenRegisterMenu(); break;
+        case 0: _t->GotoTwo(); break;
+        case 1: _t->GotoOne(); break;
+        case 2: _t->GotoSettings(); break;
+        case 3: _t->setUser((*reinterpret_cast< QString(*)>(_a[1]))); break;
         default: ;
         }
     }
-    Q_UNUSED(_a);
 }
 
-const QMetaObject MainMenu_Widget::staticMetaObject = {
-    { &QWidget::staticMetaObject, qt_meta_stringdata_MainMenu_Widget.data,
-      qt_meta_data_MainMenu_Widget,  qt_static_metacall, 0, 0}
+const QMetaObject MainScreen::staticMetaObject = {
+    { &QWidget::staticMetaObject, qt_meta_stringdata_MainScreen.data,
+      qt_meta_data_MainScreen,  qt_static_metacall, 0, 0}
 };
 
 
-const QMetaObject *MainMenu_Widget::metaObject() const
+const QMetaObject *MainScreen::metaObject() const
 {
     return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
 }
 
-void *MainMenu_Widget::qt_metacast(const char *_clname)
+void *MainScreen::qt_metacast(const char *_clname)
 {
     if (!_clname) return 0;
-    if (!strcmp(_clname, qt_meta_stringdata_MainMenu_Widget.stringdata))
-        return static_cast<void*>(const_cast< MainMenu_Widget*>(this));
+    if (!strcmp(_clname, qt_meta_stringdata_MainScreen.stringdata))
+        return static_cast<void*>(const_cast< MainScreen*>(this));
     return QWidget::qt_metacast(_clname);
 }
 
-int MainMenu_Widget::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+int MainScreen::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
 {
     _id = QWidget::qt_metacall(_c, _id, _a);
     if (_id < 0)
         return _id;
     if (_c == QMetaObject::InvokeMetaMethod) {
-        if (_id < 2)
+        if (_id < 4)
             qt_static_metacall(this, _c, _id, _a);
-        _id -= 2;
+        _id -= 4;
     } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
-        if (_id < 2)
+        if (_id < 4)
             *reinterpret_cast<int*>(_a[0]) = -1;
-        _id -= 2;
+        _id -= 4;
     }
     return _id;
 }
