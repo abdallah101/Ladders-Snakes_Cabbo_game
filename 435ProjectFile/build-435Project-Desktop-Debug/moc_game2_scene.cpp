5d9f92876703da927d56e61649b74ba33ac37e87
abdallah dandashli <ayd06@mail.aub.edu>
2019-11-17 18:04:13 +0200

    buld file for conveniance

--- 435ProjectFile/build-435Project-Desktop-Debug/moc_game2_scene.cpp
+++ 435ProjectFile/build-435Project-Desktop-Debug/moc_game2_scene.cpp
@@ -19,8 +19,8 @@
 
 QT_BEGIN_MOC_NAMESPACE
 struct qt_meta_stringdata_game2_scene_t {
-    QByteArrayData data[4];
-    char stringdata[24];
+    QByteArrayData data[6];
+    char stringdata[38];
 };
 #define QT_MOC_LITERAL(idx, ofs, len) \
     Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
@@ -32,9 +32,11 @@ static const qt_meta_stringdata_game2_scene_t qt_meta_stringdata_game2_scene = {
 QT_MOC_LITERAL(0, 0, 11),
 QT_MOC_LITERAL(1, 12, 7),
 QT_MOC_LITERAL(2, 20, 0),
-QT_MOC_LITERAL(3, 21, 1)
+QT_MOC_LITERAL(3, 21, 1),
+QT_MOC_LITERAL(4, 23, 6),
+QT_MOC_LITERAL(5, 30, 6)
     },
-    "game2_scene\0setUser\0\0a\0"
+    "game2_scene\0setUser\0\0a\0change\0update\0"
 };
 #undef QT_MOC_LITERAL
 
@@ -44,7 +46,7 @@ static const uint qt_meta_data_game2_scene[] = {
        7,       // revision
        0,       // classname
        0,    0, // classinfo
-       1,   14, // methods
+       3,   14, // methods
        0,    0, // properties
        0,    0, // enums/sets
        0,    0, // constructors
@@ -52,10 +54,14 @@ static const uint qt_meta_data_game2_scene[] = {
        0,       // signalCount
 
  // slots: name, argc, parameters, tag, flags
-       1,    1,   19,    2, 0x0a,
+       1,    1,   29,    2, 0x0a,
+       4,    0,   32,    2, 0x0a,
+       5,    0,   33,    2, 0x0a,
 
  // slots: parameters
     QMetaType::Void, QMetaType::QString,    3,
+    QMetaType::Void,
+    QMetaType::Void,
 
        0        // eod
 };
@@ -66,6 +72,8 @@ void game2_scene::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id,
         game2_scene *_t = static_cast<game2_scene *>(_o);
         switch (_id) {
         case 0: _t->setUser((*reinterpret_cast< QString(*)>(_a[1]))); break;
+        case 1: _t->change(); break;
+        case 2: _t->update(); break;
         default: ;
         }
     }
@@ -96,13 +104,13 @@ int game2_scene::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
     if (_id < 0)
         return _id;
     if (_c == QMetaObject::InvokeMetaMethod) {
-        if (_id < 1)
+        if (_id < 3)
             qt_static_metacall(this, _c, _id, _a);
-        _id -= 1;
+        _id -= 3;
     } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
-        if (_id < 1)
+        if (_id < 3)
             *reinterpret_cast<int*>(_a[0]) = -1;
-        _id -= 1;
+        _id -= 3;
     }
     return _id;
 }
